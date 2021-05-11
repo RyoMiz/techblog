d9df18e9bec3d28feb83f503ee6ea8ae5eb4f048
Ryosuke Mizuno <ryo.mizuno502@gmail.com>
2021-05-11 22:40:13 +0900

    [Fix]
    モバイルデザインの完成

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { AppProps } from 'next/app'
 
 import 'destyle.css'
+import 'swiper/swiper.scss'
+import 'swiper/components/navigation/navigation.scss'
+import 'swiper/components/pagination/pagination.scss'
+
 import 'styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
