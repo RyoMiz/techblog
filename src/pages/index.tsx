7874bc8c3d6dc98e985b4980e16b300907be1e67
Ryosuke Mizuno <ryo.mizuno502@gmail.com>
2021-05-11 09:12:30 +0900

    Initial Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+    </>
+  )
+}
+
+export default Home
